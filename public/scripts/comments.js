c3383b18a086d322d11982e4f3961731cd062582
jeongsangtae <wjdtkdxo55@naver.com>
2023-02-17 06:37:41 +0900

    NodeJS & MongoDB - DOM update based on response

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,10 +1,31 @@
 const loadCommentsBtn = document.getElementById("load-comments-btn");
+const commentsSection = document.getElementById("comments");
+
+function createCommentList(comments) {
+  const commentsList = document.createElement("ol");
+
+  for (const comment of comments) {
+    const commentElement = document.createElement("li");
+    commentElement.innerHTML = `
+      <article class="comment-item">
+        <h2>${comment.title}</h2>
+        <p>${comment.text}</p>
+      </article>
+  `;
+    commentsList.appendChild(commentElement);
+  }
+
+  return commentsList;
+}
 
 async function fetchCommentsForPost() {
   const postId = loadCommentsBtn.dataset.postid;
   const response = await fetch(`/posts/${postId}/comments`);
   const responseData = await response.json();
-  console.log(responseData);
+
+  const commentsList = createCommentList(responseData);
+  commentsSection.innerHTML = "";
+  commentsSection.appendChild(commentsList);
 }
 
 loadCommentsBtn.addEventListener("click", fetchCommentsForPost);
